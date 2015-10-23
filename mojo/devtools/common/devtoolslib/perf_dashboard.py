7615d79003f86ad1958e4a0c2d9a613d01a50569
Przemyslaw Pietrzkiewicz <ppi@chromium.org>
2015-10-23 13:04:41 +0200

    mojo_benchmark: improve recording the chart data for perf dashboard.
    This patch:
     - adds the neccessary metadata to `chart_data` being recorded
     - records individual `chart_data` for individual benchmarks (so that
       all measurements won't be tracked on one collective chart)
     - fixes an import error introduced in
       64736fae560422778d97e5e747458e34697cd24e
    
    R=etiennej@chromium.org
    
    Review URL: https://codereview.chromium.org/1412113003 .

--- mojo/devtools/common/devtoolslib/perf_dashboard.py
+++ mojo/devtools/common/devtoolslib/perf_dashboard.py
@@ -13,14 +13,16 @@ See http://www.chromium.org/developers/speed-infra/performance-dashboard/sending
 import json
 from collections import defaultdict
 
+
 class ChartDataRecorder(object):
   """Allows one to record measurement values one by one and then generate the
   JSON string that represents them in the 'chart_data' format expected by the
   performance dashboard.
   """
 
-  def __init__(self):
+  def __init__(self, benchmark_name):
     self.charts = defaultdict(list)
+    self.benchmark_name = benchmark_name
 
   def record_scalar(self, chart_name, value_name, units, value):
     """Records a single measurement value of a scalar type."""
@@ -31,5 +33,11 @@ class ChartDataRecorder(object):
         'value': value})
 
   def get_json(self):
-    """Returns the JSON string representing the recorded chart data."""
-    return json.dumps(self.charts)
+    """Returns the JSON string representing the recorded chart data, wrapping
+    it with the required meta data."""
+    chart_data = {
+        'format_version': '1.0',
+        'benchmark_name': self.benchmark_name,
+        'charts': self.charts
+    }
+    return json.dumps(chart_data)

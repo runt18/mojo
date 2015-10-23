runt18/mojo

commit 64736fae560422778d97e5e747458e34697cd24e

Teach mojo_benchmark to produce chart_data for the perf dashboard.
This patch adds the ability to encode the benchmark results in the 'chart_data'
format understood by Chromium Performance Benchmark.

Fixes domokit/devtools#52.

R=viettrungluu@chromium.org

Review URL: https://codereview.chromium.org/1406063002 .

mojo/devtools/common/devtoolslib/perf_dashboard.py
@@ -1,0 +1,35 @@
+# Copyright 2015 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# Disable the line-too-long warning.
+# pylint: disable=C0301
+"""This module implements the Chromium Performance Dashboard JSON v1.0 data
+format.
+
+See http://www.chromium.org/developers/speed-infra/performance-dashboard/sending-data-to-the-performance-dashboard.
+"""
+
+import json
+from collections import defaultdict
+
+class ChartDataRecorder(object):
+  """Allows one to record measurement values one by one and then generate the
+  JSON string that represents them in the 'chart_data' format expected by the
+  performance dashboard.
+  """
+
+  def __init__(self):
+    self.charts = defaultdict(list)
+
+  def record_scalar(self, chart_name, value_name, units, value):
+    """Records a single measurement value of a scalar type."""
+    self.charts[chart_name].append({
+        'type': 'scalar',
+        'name': value_name,
+        'units': units,
+        'value': value})
+
+  def get_json(self):
+    """Returns the JSON string representing the recorded chart data."""
+    return json.dumps(self.charts)

mojo/devtools/common/mojo_benchmark
@@ -14,6 +14,7 @@ import re
 
 from devtoolslib import shell_arguments
 from devtoolslib import shell_config
+from devtoolslib import performance_dashboard
 
 
 _DESCRIPTION = """Runner for Mojo application benchmarks.
@@ -175,6 +176,9 @@ def main():
                       help='a file listing benchmarks to run')
   parser.add_argument('--save-traces', action='store_true',
                       help='save the traces produced by benchmarks to disk')
+  parser.add_argument('--chart-data-output-file', type=argparse.FileType('w'),
+                      help='file to write chart data for the performance '
+                      'dashboard to')
 
   # Common shell configuration arguments.
   shell_config.add_shell_arguments(parser)
@@ -190,6 +194,10 @@ def main():
   target_os = 'android' if script_args.android else 'linux'
   benchmark_list_params = {"target_os": target_os}
   exec script_args.benchmark_list_file in benchmark_list_params
+
+  chart_data_recorder = None
+  if script_args.chart_data_output_file:
+    chart_data_recorder = performance_dashboard.ChartDataRecorder()
 
   exit_code = 0
   for benchmark_spec in benchmark_list_params['benchmarks']:
@@ -213,8 +221,13 @@ def main():
         # results and preserve the required order.
         for measurement_spec in measurements:
           if measurement_spec in measurement_results:
-            print '%s %s' % (measurement_spec,
-                             measurement_results[measurement_spec])
+            result = measurement_results[measurement_spec]
+            print '%s %s' % (measurement_spec, result)
+
+            if chart_data_recorder:
+              measurement_name = measurement_spec.replace('/', '-')
+              chart_data_recorder.record_scalar(name, measurement_name, 'ms',
+                                                result)
           else:
             print '%s ?' % measurement_spec
             some_measurements_failed = True
@@ -229,6 +242,9 @@ def main():
         print output
         print '-' * 72
         exit_code = 1
+
+  if script_args.chart_data_output_file:
+    script_args.chart_data_output_file.write(chart_data_recorder.get_json())
 
   return exit_code
 

mojo/devtools/common/devtoolslib/perf_dashboard_unittest.py
@@ -1,0 +1,68 @@
+# Copyright 2015 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Tests for the Chromium Performance Dashboard data format implementation."""
+
+import imp
+import json
+import os.path
+import sys
+import unittest
+
+try:
+  imp.find_module("devtoolslib")
+except ImportError:
+  sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from devtoolslib.perf_dashboard import ChartDataRecorder
+
+class ChartDataRecorderTest(unittest.TestCase):
+  """Tests the chart data recorder."""
+
+  def test_empty(self):
+    """Tests chart data with no charts."""
+    recorder = ChartDataRecorder()
+    result = json.loads(recorder.get_json())
+    self.assertEquals(0, len(result))
+
+  def test_one_chart(self):
+    """Tests chart data with two samples in one chart."""
+    recorder = ChartDataRecorder()
+    recorder.record_scalar('chart', 'val1', 'ms', 1)
+    recorder.record_scalar('chart', 'val2', 'ms', 2)
+
+    result = json.loads(recorder.get_json())
+    self.assertEquals(1, len(result))
+    self.assertEquals(2, len(result['chart']))
+    self.assertEquals({
+        'type': 'scalar',
+        'name': 'val1',
+        'units': 'ms',
+        'value': 1}, result['chart'][0])
+    self.assertEquals({
+        'type': 'scalar',
+        'name': 'val2',
+        'units': 'ms',
+        'value': 2}, result['chart'][1])
+
+  def test_two_charts(self):
+    """Tests chart data with two samples over two charts."""
+    recorder = ChartDataRecorder()
+    recorder.record_scalar('chart1', 'val1', 'ms', 1)
+    recorder.record_scalar('chart2', 'val2', 'ms', 2)
+
+    result = json.loads(recorder.get_json())
+    self.assertEquals(2, len(result))
+    self.assertEquals(1, len(result['chart1']))
+    self.assertEquals({
+        'type': 'scalar',
+        'name': 'val1',
+        'units': 'ms',
+        'value': 1}, result['chart1'][0])
+    self.assertEquals(1, len(result['chart2']))
+    self.assertEquals({
+        'type': 'scalar',
+        'name': 'val2',
+        'units': 'ms',
+        'value': 2}, result['chart2'][0])

commit 7615d79003f86ad1958e4a0c2d9a613d01a50569

mojo_benchmark: improve recording the chart data for perf dashboard.
This patch:
 - adds the neccessary metadata to `chart_data` being recorded
 - records individual `chart_data` for individual benchmarks (so that
   all measurements won't be tracked on one collective chart)
 - fixes an import error introduced in
   64736fae560422778d97e5e747458e34697cd24e

R=etiennej@chromium.org

Review URL: https://codereview.chromium.org/1412113003 .

mojo/devtools/common/devtoolslib/perf_dashboard.py
@@ -13,14 +13,16 @@ See http://www.chromium.org/developers/speed-infra/performance-dashboard/sending
 import json
 from collections import defaultdict
 
+
 class ChartDataRecorder(object):
   """Allows one to record measurement values one by one and then generate the
   JSON string that represents them in the 'chart_data' format expected by the
   performance dashboard.
   """
 
-  def __init__(self):
+  def __init__(self, benchmark_name):
     self.charts = defaultdict(list)
+    self.benchmark_name = benchmark_name
 
   def record_scalar(self, chart_name, value_name, units, value):
     """Records a single measurement value of a scalar type."""
@@ -31,5 +33,11 @@ class ChartDataRecorder(object):
         'value': value})
 
   def get_json(self):
-    """Returns the JSON string representing the recorded chart data."""
-    return json.dumps(self.charts)
+    """Returns the JSON string representing the recorded chart data, wrapping
+    it with the required meta data."""
+    chart_data = {
+        'format_version': '1.0',
+        'benchmark_name': self.benchmark_name,
+        'charts': self.charts
+    }
+    return json.dumps(chart_data)

mojo/devtools/common/mojo_benchmark
@@ -14,7 +14,7 @@ import re
 
 from devtoolslib import shell_arguments
 from devtoolslib import shell_config
-from devtoolslib import performance_dashboard
+from devtoolslib import perf_dashboard
 
 
 _DESCRIPTION = """Runner for Mojo application benchmarks.
@@ -195,10 +195,6 @@ def main():
   benchmark_list_params = {"target_os": target_os}
   exec script_args.benchmark_list_file in benchmark_list_params
 
-  chart_data_recorder = None
-  if script_args.chart_data_output_file:
-    chart_data_recorder = performance_dashboard.ChartDataRecorder()
-
   exit_code = 0
   for benchmark_spec in benchmark_list_params['benchmarks']:
     for variant_spec in _generate_benchmark_variants(benchmark_spec):
@@ -207,6 +203,10 @@ def main():
       duration = variant_spec['duration']
       shell_args = variant_spec.get('shell-args', []) + common_shell_args
       measurements = variant_spec['measurements']
+
+      chart_data_recorder = None
+      if script_args.chart_data_output_file:
+        chart_data_recorder = perf_dashboard.ChartDataRecorder(name)
       benchmark_succeeded, benchmark_error, output = _run_benchmark(
           shell, shell_args, name, app, duration, measurements,
           script_args.verbose, script_args.android,
@@ -243,8 +243,9 @@ def main():
         print '-' * 72
         exit_code = 1
 
-  if script_args.chart_data_output_file:
-    script_args.chart_data_output_file.write(chart_data_recorder.get_json())
+      if script_args.chart_data_output_file:
+        script_args.chart_data_output_file.write(chart_data_recorder.get_json())
+        script_args.chart_data_output_file.write('\n')
 
   return exit_code
 

mojo/devtools/common/devtoolslib/perf_dashboard_unittest.py
@@ -17,52 +17,64 @@ except ImportError:
 
 from devtoolslib.perf_dashboard import ChartDataRecorder
 
+
 class ChartDataRecorderTest(unittest.TestCase):
   """Tests the chart data recorder."""
 
   def test_empty(self):
     """Tests chart data with no charts."""
-    recorder = ChartDataRecorder()
+    recorder = ChartDataRecorder('benchmark')
     result = json.loads(recorder.get_json())
-    self.assertEquals(0, len(result))
+    self.assertEquals({
+        'format_version': '1.0',
+        'benchmark_name': 'benchmark',
+        'charts': {}}, result)
 
   def test_one_chart(self):
     """Tests chart data with two samples in one chart."""
-    recorder = ChartDataRecorder()
+    recorder = ChartDataRecorder('benchmark')
     recorder.record_scalar('chart', 'val1', 'ms', 1)
     recorder.record_scalar('chart', 'val2', 'ms', 2)
 
     result = json.loads(recorder.get_json())
-    self.assertEquals(1, len(result))
-    self.assertEquals(2, len(result['chart']))
+    self.assertEquals('1.0', result['format_version'])
+    self.assertEquals('benchmark', result['benchmark_name'])
+
+    charts = result['charts']
+    self.assertEquals(1, len(charts))
+    self.assertEquals(2, len(charts['chart']))
     self.assertEquals({
         'type': 'scalar',
         'name': 'val1',
         'units': 'ms',
-        'value': 1}, result['chart'][0])
+        'value': 1}, charts['chart'][0])
     self.assertEquals({
         'type': 'scalar',
         'name': 'val2',
         'units': 'ms',
-        'value': 2}, result['chart'][1])
+        'value': 2}, charts['chart'][1])
 
   def test_two_charts(self):
     """Tests chart data with two samples over two charts."""
-    recorder = ChartDataRecorder()
+    recorder = ChartDataRecorder('benchmark')
     recorder.record_scalar('chart1', 'val1', 'ms', 1)
     recorder.record_scalar('chart2', 'val2', 'ms', 2)
 
     result = json.loads(recorder.get_json())
-    self.assertEquals(2, len(result))
-    self.assertEquals(1, len(result['chart1']))
+    self.assertEquals('1.0', result['format_version'])
+    self.assertEquals('benchmark', result['benchmark_name'])
+
+    charts = result['charts']
+    self.assertEquals(2, len(charts))
+    self.assertEquals(1, len(charts['chart1']))
     self.assertEquals({
         'type': 'scalar',
         'name': 'val1',
         'units': 'ms',
-        'value': 1}, result['chart1'][0])
-    self.assertEquals(1, len(result['chart2']))
+        'value': 1}, charts['chart1'][0])
+    self.assertEquals(1, len(charts['chart2']))
     self.assertEquals({
         'type': 'scalar',
         'name': 'val2',
         'units': 'ms',
-        'value': 2}, result['chart2'][0])
+        'value': 2}, charts['chart2'][0])