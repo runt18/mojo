5b3e2434790f89783e5e20c358d3bdaf08025a25
Viet-Trung Luu <viettrungluu@chromium.org>
2015-10-23 11:13:46 -0700

    Convert MutexTest to use std::thread.
    R=jamesr@chromium.org
    
    Review URL: https://codereview.chromium.org/1423743004 .

--- mojo/edk/system/mutex_unittest.cc
+++ mojo/edk/system/mutex_unittest.cc
@@ -6,9 +6,9 @@
 
 #include <stdlib.h>
 
-#include "base/threading/platform_thread.h"
+#include <thread>
+
 #include "mojo/edk/system/test_utils.h"
-#include "mojo/public/cpp/system/macros.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace mojo {
@@ -22,49 +22,33 @@ void EpsilonRandomSleep() {
 
 // Basic test to make sure that Lock()/Unlock()/TryLock() don't crash ----------
 
-class BasicMutexTestThread : public base::PlatformThread::Delegate {
- public:
-  explicit BasicMutexTestThread(Mutex* mutex) : mutex_(mutex), acquired_(0) {}
-
-  void ThreadMain() override {
-    for (int i = 0; i < 10; i++) {
-      mutex_->Lock();
-      mutex_->AssertHeld();
-      acquired_++;
-      mutex_->Unlock();
-    }
-    for (int i = 0; i < 10; i++) {
-      mutex_->Lock();
-      mutex_->AssertHeld();
-      acquired_++;
-      EpsilonRandomSleep();
-      mutex_->Unlock();
-    }
-    for (int i = 0; i < 10; i++) {
-      if (mutex_->TryLock()) {
-        mutex_->AssertHeld();
-        acquired_++;
-        EpsilonRandomSleep();
-        mutex_->Unlock();
-      }
-    }
-  }
-
-  int acquired() const { return acquired_; }
-
- private:
-  Mutex* mutex_;
-  int acquired_;
-
-  MOJO_DISALLOW_COPY_AND_ASSIGN(BasicMutexTestThread);
-};
-
 TEST(MutexTest, Basic) {
   Mutex mutex;
-  BasicMutexTestThread thread(&mutex);
-  base::PlatformThreadHandle handle;
 
-  ASSERT_TRUE(base::PlatformThread::Create(0, &thread, &handle));
+  int thread_acquired = 0;
+  auto thread = std::thread([&mutex, &thread_acquired]() {
+    for (int i = 0; i < 10; i++) {
+      mutex.Lock();
+      mutex.AssertHeld();
+      thread_acquired++;
+      mutex.Unlock();
+    }
+    for (int i = 0; i < 10; i++) {
+      mutex.Lock();
+      mutex.AssertHeld();
+      thread_acquired++;
+      EpsilonRandomSleep();
+      mutex.Unlock();
+    }
+    for (int i = 0; i < 10; i++) {
+      if (mutex.TryLock()) {
+        mutex.AssertHeld();
+        thread_acquired++;
+        EpsilonRandomSleep();
+        mutex.Unlock();
+      }
+    }
+  });
 
   int acquired = 0;
   for (int i = 0; i < 5; i++) {
@@ -96,10 +80,10 @@ TEST(MutexTest, Basic) {
     mutex.Unlock();
   }
 
-  base::PlatformThread::Join(handle);
+  thread.join();
 
   EXPECT_GE(acquired, 20);
-  EXPECT_GE(thread.acquired(), 20);
+  EXPECT_GE(thread_acquired, 20);
 }
 
 TEST(MutexTest, AssertHeld) {
@@ -118,58 +102,31 @@ TEST(MutexTest, AssertHeld) {
 
 // Test that TryLock() works as expected ---------------------------------------
 
-class TryLockTestThread : public base::PlatformThread::Delegate {
- public:
-  explicit TryLockTestThread(Mutex* mutex) : mutex_(mutex), got_lock_(false) {}
-
-  void ThreadMain() override MOJO_NO_THREAD_SAFETY_ANALYSIS {
-    got_lock_ = mutex_->TryLock();
-    if (got_lock_) {
-      mutex_->AssertHeld();
-      mutex_->Unlock();
-    }
-  }
-
-  bool got_lock() const { return got_lock_; }
-
- private:
-  Mutex* mutex_;
-  bool got_lock_;
-
-  MOJO_DISALLOW_COPY_AND_ASSIGN(TryLockTestThread);
-};
-
 TEST(MutexTest, TryLock) MOJO_NO_THREAD_SAFETY_ANALYSIS {
   Mutex mutex;
 
   ASSERT_TRUE(mutex.TryLock());
   // We now have the mutex....
 
-  // This thread will not be able to get the mutex.
   {
-    TryLockTestThread thread(&mutex);
-    base::PlatformThreadHandle handle;
-
-    ASSERT_TRUE(base::PlatformThread::Create(0, &thread, &handle));
-
-    base::PlatformThread::Join(handle);
-
-    ASSERT_FALSE(thread.got_lock());
+    // This thread will not be able to get the mutex.
+    auto thread = std::thread([&mutex]() { EXPECT_FALSE(mutex.TryLock()); });
+    thread.join();
   }
 
   mutex.Unlock();
+  // And now we don't.
 
-  // This thread will....
   {
-    TryLockTestThread thread(&mutex);
-    base::PlatformThreadHandle handle;
+    // This thread will be able to get it (and then release it).
+    auto thread = std::thread([&mutex]() {
+      EXPECT_TRUE(mutex.TryLock());
+      mutex.AssertHeld();
+      mutex.Unlock();
+    });
+    thread.join();
 
-    ASSERT_TRUE(base::PlatformThread::Create(0, &thread, &handle));
-
-    base::PlatformThread::Join(handle);
-
-    ASSERT_TRUE(thread.got_lock());
-    // But it released it....
+    // And we can take it again.
     ASSERT_TRUE(mutex.TryLock());
   }
 
@@ -178,43 +135,30 @@ TEST(MutexTest, TryLock) MOJO_NO_THREAD_SAFETY_ANALYSIS {
 
 // Tests that mutexes actually exclude -----------------------------------------
 
-class MutexLockTestThread : public base::PlatformThread::Delegate {
- public:
-  MutexLockTestThread(Mutex* mutex, int* value)
-      : mutex_(mutex), value_(value) {}
-
-  // Static helper which can also be called from the main thread.
-  static void DoStuff(Mutex* mutex, int* value) {
-    for (int i = 0; i < 40; i++) {
-      mutex->Lock();
-      int v = *value;
-      EpsilonRandomSleep();
-      *value = v + 1;
-      mutex->Unlock();
-    }
+// We'll call this from both the main thread and secondary threads.
+void DoStuffWithMutex(Mutex* mutex, int* value) {
+  for (int i = 0; i < 40; i++) {
+    mutex->Lock();
+    int v = *value;
+    EpsilonRandomSleep();
+    *value = v + 1;
+    mutex->Unlock();
   }
+}
 
-  void ThreadMain() override { DoStuff(mutex_, value_); }
-
- private:
-  Mutex* mutex_;
-  int* value_;
-
-  MOJO_DISALLOW_COPY_AND_ASSIGN(MutexLockTestThread);
-};
+std::thread MakeMutexTestThread(Mutex* mutex, int* value) {
+  return std::thread([mutex, value]() { DoStuffWithMutex(mutex, value); });
+}
 
 TEST(MutexTest, MutexTwoThreads) {
   Mutex mutex;
   int value = 0;
 
-  MutexLockTestThread thread(&mutex, &value);
-  base::PlatformThreadHandle handle;
+  std::thread thread = MakeMutexTestThread(&mutex, &value);
 
-  ASSERT_TRUE(base::PlatformThread::Create(0, &thread, &handle));
+  DoStuffWithMutex(&mutex, &value);
 
-  MutexLockTestThread::DoStuff(&mutex, &value);
-
-  base::PlatformThread::Join(handle);
+  thread.join();
 
   EXPECT_EQ(2 * 40, value);
 }
@@ -223,22 +167,15 @@ TEST(MutexTest, MutexFourThreads) {
   Mutex mutex;
   int value = 0;
 
-  MutexLockTestThread thread1(&mutex, &value);
-  MutexLockTestThread thread2(&mutex, &value);
-  MutexLockTestThread thread3(&mutex, &value);
-  base::PlatformThreadHandle handle1;
-  base::PlatformThreadHandle handle2;
-  base::PlatformThreadHandle handle3;
+  std::thread thread1 = MakeMutexTestThread(&mutex, &value);
+  std::thread thread2 = MakeMutexTestThread(&mutex, &value);
+  std::thread thread3 = MakeMutexTestThread(&mutex, &value);
 
-  ASSERT_TRUE(base::PlatformThread::Create(0, &thread1, &handle1));
-  ASSERT_TRUE(base::PlatformThread::Create(0, &thread2, &handle2));
-  ASSERT_TRUE(base::PlatformThread::Create(0, &thread3, &handle3));
+  DoStuffWithMutex(&mutex, &value);
 
-  MutexLockTestThread::DoStuff(&mutex, &value);
-
-  base::PlatformThread::Join(handle1);
-  base::PlatformThread::Join(handle2);
-  base::PlatformThread::Join(handle3);
+  thread1.join();
+  thread2.join();
+  thread3.join();
 
   EXPECT_EQ(4 * 40, value);
 }
