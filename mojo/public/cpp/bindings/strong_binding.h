13a725c0620592b5e8446b828ebfe7762e846c6b
Jeff Brown <jeffbrown@google.com>
2015-10-26 15:37:52 -0700

    Make StrongBinding API more consistent with Binding.
    Added some methods and documentation which were present on Binding but were missing from StrongBinding.
    
    Clarified certain aspects of the StrongBinding ownership model in the documentation.
    
    Review URL: https://codereview.chromium.org/1413193005 .

--- mojo/public/cpp/bindings/strong_binding.h
+++ mojo/public/cpp/bindings/strong_binding.h
@@ -23,6 +23,12 @@ namespace mojo {
 // connection error is detected the implementation is deleted. Deleting the
 // connector also closes the pipe.
 //
+// The strong binding does not actually take ownership of the implementation.
+// If the implementation is not bound to a message pipe or if the message
+// pipe is closed locally then no connection error will be produced and the
+// implementation will therefore not be deleted by the strong binding.
+// Keep this in mind to avoid unintentional memory leaks.
+//
 // Example of an implementation that is always bound strongly to a pipe
 //
 //   class StronglyBound : public Foo {
@@ -46,10 +52,16 @@ namespace mojo {
 template <typename Interface>
 class StrongBinding {
  public:
+  // Constructs an incomplete binding that will use the implementation |impl|.
+  // The binding may be completed with a subsequent call to the |Bind| method.
+  // Does not take ownership of |impl|, which must outlive the binding.
   explicit StrongBinding(Interface* impl) : binding_(impl) {
     binding_.set_connection_error_handler([this]() { OnConnectionError(); });
   }
 
+  // Constructs a completed binding of message pipe |handle| to implementation
+  // |impl|. Does not take ownership of |impl|, which must outlive the binding.
+  // See class comment for definition of |waiter|.
   StrongBinding(
       Interface* impl,
       ScopedMessagePipeHandle handle,
@@ -58,6 +70,12 @@ class StrongBinding {
     binding_.Bind(handle.Pass(), waiter);
   }
 
+  // Constructs a completed binding of |impl| to a new message pipe, passing the
+  // client end to |ptr|, which takes ownership of it. The caller is expected to
+  // pass |ptr| on to the client of the service. Does not take ownership of any
+  // of the parameters. |impl| must outlive the binding. |ptr| only needs to
+  // last until the constructor returns. See class comment for definition of
+  // |waiter|.
   StrongBinding(
       Interface* impl,
       InterfacePtr<Interface>* ptr,
@@ -66,6 +84,10 @@ class StrongBinding {
     binding_.Bind(ptr, waiter);
   }
 
+  // Constructs a completed binding of |impl| to the message pipe endpoint in
+  // |request|, taking ownership of the endpoint. Does not take ownership of
+  // |impl|, which must outlive the binding. See class comment for definition of
+  // |waiter|.
   StrongBinding(
       Interface* impl,
       InterfaceRequest<Interface> request,
@@ -74,8 +96,13 @@ class StrongBinding {
     binding_.Bind(request.Pass(), waiter);
   }
 
+  // Tears down the binding, closing the message pipe and leaving the interface
+  // implementation unbound.  Does not cause the implementation to be deleted.
   ~StrongBinding() {}
 
+  // Completes a binding that was constructed with only an interface
+  // implementation. Takes ownership of |handle| and binds it to the previously
+  // specified implementation. See class comment for definition of |waiter|.
   void Bind(
       ScopedMessagePipeHandle handle,
       const MojoAsyncWaiter* waiter = Environment::GetDefaultAsyncWaiter()) {
@@ -83,6 +110,12 @@ class StrongBinding {
     binding_.Bind(handle.Pass(), waiter);
   }
 
+  // Completes a binding that was constructed with only an interface
+  // implementation by creating a new message pipe, binding one end of it to the
+  // previously specified implementation, and passing the other to |ptr|, which
+  // takes ownership of it. The caller is expected to pass |ptr| on to the
+  // eventual client of the service. Does not take ownership of |ptr|. See
+  // class comment for definition of |waiter|.
   void Bind(
       InterfacePtr<Interface>* ptr,
       const MojoAsyncWaiter* waiter = Environment::GetDefaultAsyncWaiter()) {
@@ -90,6 +123,10 @@ class StrongBinding {
     binding_.Bind(ptr, waiter);
   }
 
+  // Completes a binding that was constructed with only an interface
+  // implementation by removing the message pipe endpoint from |request| and
+  // binding it to the previously specified implementation. See class comment
+  // for definition of |waiter|.
   void Bind(
       InterfaceRequest<Interface> request,
       const MojoAsyncWaiter* waiter = Environment::GetDefaultAsyncWaiter()) {
@@ -97,16 +134,46 @@ class StrongBinding {
     binding_.Bind(request.Pass(), waiter);
   }
 
-  bool WaitForIncomingMethodCall() {
-    return binding_.WaitForIncomingMethodCall();
+  // Blocks the calling thread until either a call arrives on the previously
+  // bound message pipe, the deadline is exceeded, or an error occurs. Returns
+  // true if a method was successfully read and dispatched.
+  bool WaitForIncomingMethodCall(
+      MojoDeadline deadline = MOJO_DEADLINE_INDEFINITE) {
+    return binding_.WaitForIncomingMethodCall(deadline);
   }
 
-  // Note: The error handler must not delete the interface implementation.
+  // Closes the message pipe that was previously bound.  Does not cause the
+  // implementation to be deleted.
+  void Close() { binding_->Close(); }
+
+  // Unbinds the underlying pipe from this binding and returns it so it can be
+  // used in another context, such as on another thread or with a different
+  // implementation. Put this object into a state where it can be rebound to a
+  // new pipe.  Does not cause the implementation to be deleted.
+  InterfaceRequest<Interface> Unbind() { return binding_->Unbind(); }
+
+  // Sets an error handler that will be called if a connection error occurs on
+  // the bound message pipe.  Note: The error handler must not delete the
+  // interface implementation since that will happen immediately after the
+  // error handler returns.
   void set_connection_error_handler(const Closure& error_handler) {
     connection_error_handler_ = error_handler;
   }
 
+  // Returns the interface implementation that was previously specified. Caller
+  // does not take ownership.
   Interface* impl() { return binding_.impl(); }
+
+  // Indicates whether the binding has been completed (i.e., whether a message
+  // pipe has been bound to the implementation).
+  bool is_bound() const { return binding_.is_bound(); }
+
+  // Returns the value of the handle currently bound to this Binding which can
+  // be used to make explicit Wait/WaitMany calls. Requires that the Binding be
+  // bound. Ownership of the handle is retained by the Binding, it is not
+  // transferred to the caller.
+  MessagePipeHandle handle() const { return binding_.handle(); }
+
   // Exposed for testing, should not generally be used.
   internal::Router* internal_router() { return binding_.internal_router(); }
 
@@ -119,7 +186,7 @@ class StrongBinding {
   Closure connection_error_handler_;
   Binding<Interface> binding_;
 
-  MOJO_MOVE_ONLY_TYPE(StrongBinding);
+  MOJO_DISALLOW_COPY_AND_ASSIGN(StrongBinding);
 };
 
 }  // namespace mojo
