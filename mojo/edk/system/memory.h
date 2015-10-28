8b1e583c33fd0edf51c9e35edaf2bad5ae89a3b5
Viet-Trung Luu <viettrungluu@chromium.org>
2015-10-28 14:25:49 -0700

    EDK: Fix UserOptionsReader.
    Basically, the problem is that things like the following aren't safe:
    
    // Fields other than |size| are optional; |size| may be 4, 8, 12, etc.
    struct Foo {
      uint32_t size;
      uint32_t more_stuff_1;
      uint32_t more_stuff_2;
      // etc.
    };
    
    // |buffer| points to a "Foo" like above (with "optional" fields).
    void DoFoo(const void* buffer) {
      const Foo* foo = static_cast<const Foo*>(buffer);
      if (foo->size < 8) return;
      // ... use more_stuff_1
      if (foo->size < 12) return;
      // ... use more_stuff_2
      // etc.
    }
    
    The problem occurs at the outset: Accessing |foo->size| may result in
    accessing more than just 4 bytes at |buffer| (the compiler thinks this
    is OK, since a |Foo| has the extra bytes available -- e.g., it may do an
    8-byte read and then mask off the result). (And then the problem occurs
    again with the access of every field.)
    
    So, instead of copying |buffer| to a byte array (which was already
    dubious, with respect to alignment), we copy it to a |Foo| (zeroing out
    any remaining bytes, and ignoring any specified size over
    |sizeof(Foo)|).
    
    R=vardhan@google.com
    BUG=fixes #486
    
    Review URL: https://codereview.chromium.org/1423993004 .

--- mojo/edk/system/memory.h
+++ mojo/edk/system/memory.h
@@ -58,8 +58,8 @@ template <typename Type>
 class UserPointerWriter;
 template <typename Type>
 class UserPointerReaderWriter;
-template <class Options>
-class UserOptionsReader;
+template <typename Type>
+class UserPointerPartialReader;
 
 // Provides a convenient way to implicitly get null |UserPointer<Type>|s.
 struct NullUserPointer {};
@@ -237,13 +237,25 @@ class UserPointer {
   using Writer = UserPointerWriter<Type>;
   using ReaderWriter = UserPointerReaderWriter<Type>;
 
+  // This is like |Reader| above, but for partially reading the memory of a
+  // single object (usually a struct). The pointer it provides will be to a full
+  // |Type| (no more, no less), with unavailable bytes set to zero.
+  //
+  // Note: It isn't safe to just use |UserPointer<const char>::Reader| and
+  // reinterpret cast the pointer to a struct pointer. Even if before accessing
+  // a field you check that it's within the available size, the compiler may
+  // read beyond the extent of the field itself, so long as the read is still
+  // within the struct.
+  //
+  // TODO(vtl): Add writer and reader-writer versions of this if/when necessary.
+  using PartialReader = UserPointerPartialReader<Type>;
+
  private:
   friend class UserPointerReader<Type>;
   friend class UserPointerReader<const Type>;
   friend class UserPointerWriter<Type>;
   friend class UserPointerReaderWriter<Type>;
-  template <class Options>
-  friend class UserOptionsReader;
+  friend class UserPointerPartialReader<Type>;
 
   Type* pointer_;
   // Allow copy and assignment.
@@ -266,34 +278,23 @@ class UserPointerReader {
 
   // Note: If |count| is zero, |GetPointer()| will always return null.
   UserPointerReader(UserPointer<const TypeNoConst> user_pointer, size_t count) {
-    Init(user_pointer.pointer_, count, true);
+    Init(user_pointer.pointer_, count);
   }
   UserPointerReader(UserPointer<TypeNoConst> user_pointer, size_t count) {
-    Init(user_pointer.pointer_, count, true);
+    Init(user_pointer.pointer_, count);
   }
 
   const TypeNoConst* GetPointer() const { return buffer_.get(); }
 
  private:
-  template <class Options>
-  friend class UserOptionsReader;
-
-  struct NoCheck {};
-  UserPointerReader(NoCheck,
-                    UserPointer<const TypeNoConst> user_pointer,
-                    size_t count) {
-    Init(user_pointer.pointer_, count, false);
-  }
-
-  void Init(const TypeNoConst* user_pointer, size_t count, bool check) {
+  void Init(const TypeNoConst* user_pointer, size_t count) {
     if (count == 0)
       return;
 
-    if (check) {
-      internal::CheckUserPointerWithCount<sizeof(TypeNoConst),
-                                          MOJO_ALIGNOF(TypeNoConst)>(
-          user_pointer, count);
-    }
+    internal::CheckUserPointerWithCount<sizeof(TypeNoConst),
+                                        MOJO_ALIGNOF(TypeNoConst)>(user_pointer,
+                                                                   count);
+
     buffer_.reset(new TypeNoConst[count]);
     memcpy(buffer_.get(), user_pointer, count * sizeof(TypeNoConst));
   }
@@ -368,6 +369,47 @@ class UserPointerReaderWriter {
   std::unique_ptr<Type[]> buffer_;
 
   MOJO_DISALLOW_COPY_AND_ASSIGN(UserPointerReaderWriter);
+};
+
+// Implementation of |UserPointer<Type>::PartialReader|.
+template <typename Type>
+class UserPointerPartialReader {
+ private:
+  using TypeNoConst = typename std::remove_cv<Type>::type;
+
+ public:
+  static_assert(!std::is_volatile<Type>::value, "Type must not be volatile");
+
+  // Note: If |count| is zero, |GetPointer()| will always return null.
+  UserPointerPartialReader(UserPointer<const TypeNoConst> user_pointer,
+                           size_t num_bytes) {
+    Init(user_pointer.pointer_, num_bytes);
+  }
+  UserPointerPartialReader(UserPointer<TypeNoConst> user_pointer,
+                           size_t num_bytes) {
+    Init(user_pointer.pointer_, num_bytes);
+  }
+
+  const TypeNoConst* GetPointer() const { return &storage_; }
+
+ private:
+  void Init(const TypeNoConst* user_pointer, size_t num_bytes) {
+    // Check that all |num_bytes| are valid.
+    internal::CheckUserPointerWithSize<MOJO_ALIGNOF(TypeNoConst)>(user_pointer,
+                                                                  num_bytes);
+
+    // But only copy up to |num_bytes|.
+    if (num_bytes >= sizeof(TypeNoConst))
+      num_bytes = sizeof(TypeNoConst);
+
+    memcpy(&storage_, user_pointer, num_bytes);
+    memset(reinterpret_cast<char*>(&storage_) + num_bytes, 0,
+           sizeof(TypeNoConst) - num_bytes);
+  }
+
+  TypeNoConst storage_;
+
+  MOJO_DISALLOW_COPY_AND_ASSIGN(UserPointerPartialReader);
 };
 
 }  // namespace system
