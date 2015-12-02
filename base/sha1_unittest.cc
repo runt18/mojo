a67383613f17083d918c31cf377932f5b43b91ab
Sean Klein <smklein@chromium.org>
2015-12-02 15:11:37 -0800

    Added SHA1HashFile unittests.
    A bit belated, but tests this CL:
    https://codereview.chromium.org/1449693002/
    Now that this CL has landed:
    https://codereview.chromium.org/1445163002/
    
    BUG=#396
    R=phosek@chromium.org
    
    Review URL: https://codereview.chromium.org/1460513002 .

--- base/sha1_unittest.cc
+++ base/sha1_unittest.cc
@@ -7,6 +7,7 @@
 #include <string>
 
 #include "base/basictypes.h"
+#include "base/files/scoped_file.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 TEST(SHA1Test, Test1) {
@@ -103,6 +104,67 @@ TEST(SHA1Test, Test3Bytes) {
 
   base::SHA1HashBytes(reinterpret_cast<const unsigned char*>(input.c_str()),
                       input.length(), output);
+  for (size_t i = 0; i < base::kSHA1Length; i++)
+    EXPECT_EQ(expected[i], output[i]);
+}
+
+// Helper function to test SHA1HashFile.
+void doHashFile(const std::string& input, unsigned char* output) {
+  std::string file_name = "sha1_test_data.txt";
+  base::ScopedFILE file(fopen(file_name.c_str(), "w+"));
+  EXPECT_NE(nullptr, file);
+  EXPECT_EQ(input.length(),
+            fwrite(input.c_str(), sizeof(char), input.length(), file.get()));
+  EXPECT_EQ(0, fflush(file.get()));
+  EXPECT_EQ(true, base::SHA1HashFile(file_name, output));
+  EXPECT_EQ(0, unlink(file_name.c_str()));
+}
+
+TEST(SHA1Test, Test1File) {
+  // Example A.1 from FIPS 180-2: one-block message.
+  std::string input = "abc";
+  unsigned char output[base::kSHA1Length];
+
+  const unsigned char expected[] = { 0xa9, 0x99, 0x3e, 0x36,
+                                     0x47, 0x06, 0x81, 0x6a,
+                                     0xba, 0x3e, 0x25, 0x71,
+                                     0x78, 0x50, 0xc2, 0x6c,
+                                     0x9c, 0xd0, 0xd8, 0x9d };
+
+  doHashFile(input, output);
+  for (size_t i = 0; i < base::kSHA1Length; i++)
+    EXPECT_EQ(expected[i], output[i]);
+}
+
+TEST(SHA1Test, Test2File) {
+  // Example A.2 from FIPS 180-2: multi-block message.
+  std::string input =
+      "abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq";
+  unsigned char output[base::kSHA1Length];
+
+  const unsigned char expected[] = { 0x84, 0x98, 0x3e, 0x44,
+                                     0x1c, 0x3b, 0xd2, 0x6e,
+                                     0xba, 0xae, 0x4a, 0xa1,
+                                     0xf9, 0x51, 0x29, 0xe5,
+                                     0xe5, 0x46, 0x70, 0xf1 };
+
+  doHashFile(input, output);
+  for (size_t i = 0; i < base::kSHA1Length; i++)
+    EXPECT_EQ(expected[i], output[i]);
+}
+
+TEST(SHA1Test, Test3File) {
+  // Example A.3 from FIPS 180-2: long message.
+  std::string input(1000000, 'a');
+  unsigned char output[base::kSHA1Length];
+
+  const unsigned char expected[] = { 0x34, 0xaa, 0x97, 0x3c,
+                                     0xd4, 0xc4, 0xda, 0xa4,
+                                     0xf6, 0x1e, 0xeb, 0x2b,
+                                     0xdb, 0xad, 0x27, 0x31,
+                                     0x65, 0x34, 0x01, 0x6f };
+
+  doHashFile(input, output);
   for (size_t i = 0; i < base::kSHA1Length; i++)
     EXPECT_EQ(expected[i], output[i]);
 }
