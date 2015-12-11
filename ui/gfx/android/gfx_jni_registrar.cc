9498ce03c41ba008bc95900130054318aeb7eaa2
James Robinson <jamesr@chromium.org>
2015-12-11 15:45:36 -0800

    Remove many unused files from //skia/ext
    We don't build for windows or use the custom convolvers etc.
    
    R=kulakowski@google.com, viettrungluu@chromium.org
    
    Review URL: https://codereview.chromium.org/1519243002 .

--- ui/gfx/android/gfx_jni_registrar.cc
+++ ui/gfx/android/gfx_jni_registrar.cc
@@ -6,7 +6,6 @@
 
 #include "base/android/jni_android.h"
 #include "base/android/jni_registrar.h"
-#include "ui/gfx/android/java_bitmap.h"
 #include "ui/gfx/android/shared_device_display_info.h"
 #include "ui/gfx/android/view_configuration.h"
 
@@ -16,7 +15,6 @@ namespace android {
 static base::android::RegistrationMethod kGfxRegisteredMethods[] = {
   { "SharedDeviceDisplayInfo",
       SharedDeviceDisplayInfo::RegisterSharedDeviceDisplayInfo },
-  { "JavaBitmap", JavaBitmap::RegisterJavaBitmap },
   { "ViewConfiguration", ViewConfiguration::RegisterViewConfiguration }
 };
 
