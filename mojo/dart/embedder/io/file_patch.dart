a803d7a98f6c71b96b81cbc0ea7dac01893d8fb0
John McCutchan <johnmccutchan@google.com>
2015-12-22 07:18:48 -0800

    Directory list support for dart:io
    - Implement Directory.list recursive and non-recursive.
    - Throw UnsupportedError on *Sync() variants.
    - Implement FileStat.stat
    - Implement FileSystemEntity.type
    
    R=zra@google.com
    
    Review URL: https://codereview.chromium.org/1547523002 .

--- mojo/dart/embedder/io/file_patch.dart
+++ mojo/dart/embedder/io/file_patch.dart
@@ -8,17 +8,51 @@ import 'dart:_mojo_services/mojo/files/types.mojom.dart' as types;
 // Implementation of Directory, File, and RandomAccessFile for Mojo.
 //
 
+// Helper to convert from mojo:files error to OSError.
 OSError _OSErrorFromError(types.Error error) {
   assert(error != null);
   return new OSError(error.toString(), error.toJson());
 }
 
+// All paths in mojo:files are relative to the root directory. This helper
+// strips away any leading slashes.
 String _ensurePathIsRelative(String path) {
   while (path.startsWith('/')) {
     // Trim off the leading '/'.
     path = path.substring(1);
   }
   return path;
+}
+
+// The mojo implementation of dart:io does not support any synchronous
+// file operations. This helper throws an unsupported error.
+dynamic _onSyncOperation() {
+  throw new UnsupportedError(
+      "Synchronous operations are not supported by this embedder");
+}
+
+// Convert from mojo:files Timespec to DateTime.
+DateTime _dateTimeFromTimespec(types.Timespec ts) {
+  if (ts == null) {
+    // Dawn of time.
+    return new DateTime.fromMillisecondsSinceEpoch(0);
+  }
+  int microseconds = ts.seconds * Duration.MICROSECONDS_PER_SECOND;
+  microseconds += ts.nanoseconds ~/ 1000;
+  return new DateTime.fromMicrosecondsSinceEpoch(microseconds);
+}
+
+// Convert from mojo:files FileType to FileSystemEntityType.
+FileSystemEntityType _fileSystemEntityTypeFromFileType(types.FileType ft) {
+  if (ft == types.FileType.unknown) {
+    return FileSystemEntityType.NOT_FOUND;
+  } else if (ft == types.FileType.regularFile) {
+    return FileSystemEntityType.FILE;
+  } else if (ft == types.FileType.directory) {
+    return FileSystemEntityType.DIRECTORY;
+  }
+  throw new UnimplementedError();
+  return FileSystemEntityType.NOT_FOUND;
 }
 
 patch class _Directory {
@@ -52,6 +86,8 @@ patch class _Directory {
     return this;
   }
 
+  /* patch */ void createSync({bool recursive: false}) => _onSyncOperation();
+
   /* patch */ Future<Directory> createTemp([String prefix]) async {
     DirectoryProxy rootDirectory = await _getRootDirectory();
     // Create directory and fail if it already exists.
@@ -73,6 +109,8 @@ patch class _Directory {
     return new Directory(tempPath);
   }
 
+  /* patch */ Directory createTempSync([String prefix]) => _onSyncOperation();
+
   /* patch */ Future<bool> exists() async {
     DirectoryProxy rootDirectory = await _getRootDirectory();
     int flags = types.kOpenFlagRead | types.kOpenFlagWrite;
@@ -84,6 +122,33 @@ patch class _Directory {
     // If we can open it, it exists.
     return response.error == types.Error.ok;
   }
+
+  /* patch */ bool existsSync() => _onSyncOperation();
+
+  /* patch */ Stream<FileSystemEntity> list({bool recursive: false,
+                                            bool followLinks: true}) {
+    _DirectoryLister directoryLister = new _DirectoryLister(path, recursive);
+    StreamController streamController = new StreamController();
+    directoryLister.list(streamController);
+    return streamController.stream;
+  }
+
+  /* patch */ List listSync({bool recursive: false,
+                             bool followLinks: true}) {
+    return _onSyncOperation();
+  }
+
+  /* patch */ Future<FileStat> stat() {
+    return FileStat.stat(path);
+  }
+
+  /* patch */ FileStat statSync() => _onSyncOperation();
+
+  /* patch */ Future<Directory> rename(String newPath) {
+    throw new UnimplementedError();
+  }
+
+  /* patch */ Directory renameSync(String newPath) => _onSyncOperation();
 
   /* patch */ static _current() {
     return _currentDirectoryPath;
@@ -122,6 +187,61 @@ patch class _Directory {
   }
 }
 
+
+class _DirectoryLister {
+  final String _path;
+  final bool _recursive;
+  final List<String> _directoriesToList = new List<String>();
+
+  _DirectoryLister(this._path, this._recursive);
+
+  list(StreamController streamController) async {
+    _directoriesToList.add(_path);
+
+    DirectoryProxy rootDirectory = await _getRootDirectory();
+    int flags = types.kOpenFlagRead | types.kOpenFlagWrite;
+
+    while (_directoriesToList.length > 0) {
+      // Remove head.
+      String path = _directoriesToList.removeAt(0);
+      // Open directory.
+      DirectoryProxy directory = new DirectoryProxy.unbound();
+      var response =
+          await rootDirectory.responseOrError(
+              rootDirectory.ptr.openDirectory(_ensurePathIsRelative(path),
+                                              directory,
+                                              flags));
+      if (response.error != types.Error.ok) {
+        // Skip if we can't open it.
+        continue;
+      }
+      // Read contents.
+      var readResponse = await directory.responseOrError(directory.ptr.read());
+      // We are done with the directory now.
+      directory.close(immediate: true);
+      if (readResponse.error != types.Error.ok) {
+        // Skip if we can't read it.
+        continue;
+      }
+      List<types.DirectoryEntry> directoryContents =
+          readResponse.directoryContents;
+      for (types.DirectoryEntry entry in directoryContents) {
+        String childPath = '$path/${entry.name}';
+        if (entry.type == types.FileType.directory) {
+          if (_recursive) {
+            if ((entry.name != '.') && (entry.name != '..')) {
+              _directoriesToList.add(childPath);
+            }
+          }
+          streamController.add(new Directory(childPath));
+        } else {
+          streamController.add(new File(childPath));
+        }
+      }
+    }
+    streamController.close();
+  }
+}
 
 patch class _File {
   /* patch */ Future<bool> exists() async {
@@ -208,12 +328,85 @@ patch class _File {
 }
 
 patch class FileStat {
+  /* patch */ static FileStat statSync(String path) {
+    return _onSyncOperation();
+  }
+
+  /* patch */ static Future<FileStat> stat(String path) async {
+    DirectoryProxy rootDirectory = await _getRootDirectory();
+    int flags = types.kOpenFlagRead | types.kOpenFlagWrite;
+    DirectoryProxy directory = new DirectoryProxy.unbound();
+    var response =
+        await await rootDirectory.responseOrError(
+            rootDirectory.ptr.openDirectory(_ensurePathIsRelative(path),
+                                            directory,
+                                            flags));
+    if (response.error != types.Error.ok) {
+      throw _OSErrorFromError(response.error);
+    }
+    var statResponse = await directory.responseOrError(directory.ptr.stat());
+    // We are done with the directory now.
+    directory.close(immediate: true);
+    if (statResponse.error != types.Error.ok) {
+      throw _OSErrorFromError(response.error);
+    }
+    types.FileInformation fileInformation = statResponse.fileInformation;
+    DateTime modified = _dateTimeFromTimespec(fileInformation.mtime);
+    DateTime accessed = _dateTimeFromTimespec(fileInformation.atime);
+    int size = fileInformation.size;
+    const userReadWriteExecutableUnixMode = 0x1c0;
+    FileSystemEntityType fset =
+        _fileSystemEntityTypeFromFileType(fileInformation.type);
+    return new FileStat._internal(modified,
+                                  modified,
+                                  accessed,
+                                  fset,
+                                  userReadWriteExecutableUnixMode,
+                                  size);
+  }
+
   /* patch */ static _statSync(String path) {
+    _onSyncOperation();
   }
 }
 
 
 patch class FileSystemEntity {
+  /* patch */ Future<String> resolveSymbolicLinks() {
+    // TODO(johnmccutchan): Canonicalize path before returning.
+    return path;
+  }
+
+  /* patch */ String resolveSymbolicLinksSync() {
+    return _onSyncOperation();
+  }
+
+  /* patch */ Stream<FileSystemEvent> watch({int events: FileSystemEvent.ALL,
+                                             bool recursive: false}) {
+    throw new UnsupportedError(
+        "File system watch is not supported by this embedder");
+  }
+
+  /* patch */ static Future<bool> identical(String path1, String path2) {
+    // TODO(johnmccutchan): Canonicalize paths before comparing.
+    return path1 == path2;
+  }
+
+  /* patch */ static bool identicalSync(String path1, String path2) {
+    return _onSyncOperation();
+  }
+
+  /* patch */ static Future<FileSystemEntityType> type(
+      String path, {bool followLinks: true}) async {
+    FileStat fs = await FileStat.stat(path);
+    return fs.type;
+  }
+
+  /* patch */ static FileSystemEntityType typeSync(
+      String path, {bool followLinks: true}) {
+      return _onSyncOperation();
+  }
+
   /* patch */ static _getType(String path, bool followLinks) {
     throw new UnimplementedError();
   }
--- services/dart/dart_apptests/lib/src/file_apptests.dart
+++ services/dart/dart_apptests/lib/src/file_apptests.dart
@@ -56,5 +56,66 @@ tests(Application application, String url) {
       await fLong.create();
       expect(await f.exists(), isTrue);
     });
+    test('Directory sync operation', () async {
+      Directory directory = new Directory('/foo');
+      bool exceptionCaught = false;
+      try {
+        directory.statSync();
+        // We shouldn't hit here.
+        fail("Directory.statSync is expected to throw.");
+      } on UnsupportedError catch (e) {
+        exceptionCaught = true;
+      }
+      expect(exceptionCaught, isTrue);
+    });
+    test('Directory stat', () async {
+      Directory directory =
+          await Directory.systemTemp.createTemp('stat_test');
+      FileStat fs = await directory.stat();
+      expect(fs, isNotNull);
+      expect(fs.modeString(), "rwx------");
+      expect(fs.type, equals(FileSystemEntityType.DIRECTORY));
+    });
+    test('Directory list', () async {
+      // Setup state.
+      Directory directory =
+        await Directory.systemTemp.createTemp('list_test');
+      File f = new File('${directory.path}/child_file.txt');
+      await f.create();
+      Directory d = new Directory('${directory.path}/child_dir');
+      await d.create();
+      Directory d2 = new Directory('${d.path}/deeper_child_dir');
+      await d2.create();
+
+
+      // Non-recursive listing.
+      List<String> paths = new List<String>();
+      await for (FileSystemEntity entity in directory.list()) {
+        paths.add(entity.path);
+      }
+      paths.sort();
+      expect(paths.length, 4);
+      expect(paths[0].endsWith('/.'), isTrue);
+      expect(paths[1].endsWith('/..'), isTrue);
+      expect(paths[2].endsWith('/child_dir'), isTrue);
+      expect(paths[3].endsWith('/child_file.txt'), isTrue);
+
+      // Recursive listing.
+      paths.clear();
+      await for (FileSystemEntity entity in directory.list(recursive: true)) {
+        paths.add(entity.path);
+      }
+      paths.sort();
+      expect(paths.length, 9);
+      expect(paths[0].endsWith('/.'), isTrue);
+      expect(paths[1].endsWith('/..'), isTrue);
+      expect(paths[2].endsWith('/child_dir'), isTrue);
+      expect(paths[3].endsWith('/child_dir/.'), isTrue);
+      expect(paths[4].endsWith('/child_dir/..'), isTrue);
+      expect(paths[5].endsWith('/child_dir/deeper_child_dir'), isTrue);
+      expect(paths[6].endsWith('/child_dir/deeper_child_dir/.'), isTrue);
+      expect(paths[7].endsWith('/child_dir/deeper_child_dir/..'), isTrue);
+      expect(paths[8].endsWith('/child_file.txt'), isTrue);
+    });
   });
 }
