47504e20a10980c122d1ee97891a72b651698cfa
James Robinson <jamesr@chromium.org>
2016-01-13 10:44:53 -0800

    Add script to run gclient hooks for jiri
    R=viettrungluu@chromium.org
    
    Review URL: https://codereview.chromium.org/1577373002 .

---
+++ tools/run_hooks.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# Copyright 2015 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# This script runs all of the hooks specified in DEPS in order.
+
+import os
+import subprocess
+import sys
+
+scope = {}
+
+def Var(key):
+  return scope["vars"][key]
+
+scope["Var"] = Var
+
+def main(args):
+  gclient_path = os.path.abspath(os.path.join(os.path.dirname(__file__),
+    os.pardir, os.pardir))
+  deps_path = os.path.join(gclient_path, "src", "DEPS")
+  with open(deps_path) as deps_contents:
+    d = deps_contents.read()
+  exec(d, scope)
+  for hook in scope["hooks"]:
+    name = hook["name"]
+    print "________ running '%s' in '%s'" % (" ".join(hook["action"]),
+        gclient_path)
+    subprocess.check_call(hook["action"], cwd=gclient_path)
+    print
+  return 0
+
+if __name__ == '__main__':
+  sys.exit(main(sys.argv[1:]))
